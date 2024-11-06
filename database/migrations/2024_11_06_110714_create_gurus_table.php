4975695252159ebd90241b9e096d064d2d53743e
Your Name <you@example.com>
2024-11-06 18:43:37 +0700

    add migration

---
+++ database/migrations/2024_11_06_110714_create_gurus_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gurus', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('nip', 20)->unique();
+            $table->unsignedBigInteger('kelas_id')->nullable(); // Foreign key ke tabel kelas
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('kelas_id')->references('id')->on('kelas')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gurus');
+    }
+};
