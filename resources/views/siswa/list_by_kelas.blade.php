33b89766150ab599d702a3cd7b60d6a0d222709d
Your Name <you@example.com>
2024-11-08 11:14:07 +0700

    add view siswa by kelas

---
+++ resources/views/siswa/list_by_kelas.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+@section('title', 'List Siswa Berdasarkan Kelas')
+
+@section('content')
+    <div class="card shadow-sm">
+        <div class="card-header">
+            <h1 class="card-title fs-4">List Siswa Berdasarkan Kelas</h1>
+        </div>
+        <div class="card-body">
+            <div class="card card-p-0 card-flush">
+                <div class="card-body p-2">
+                    <table class="table align-middle border rounded table-sm fs-7" id="kelas_siswa_table">
+                        <thead>
+                            <tr class="text-start text-gray-500 fw-bold text-uppercase">
+                                <th class="px-2 text-center">No</th>
+                                <th class="px-2 text-start">Nama Kelas</th>
+                                <th class="px-2 text-start">Data Siswa</th>
+                            </tr>
+                        </thead>
+                        <tbody class="fw-semibold text-gray-600">
+                            @foreach ($kelasList as $index => $kelas)
+                                <tr>
+                                    <td class="px-2 text-center">{{ $index + 1 }}</td>
+                                    <td class="px-2 text-start">{{ $kelas->nama_kelas }}</td>
+                                    <td class="px-2 text-start">
+                                        <ul>
+                                            @foreach ($kelas->siswa as $siswa)
+                                                <li>{{ $siswa->nama }} ({{ $siswa->nis }})</li>
+                                            @endforeach
+                                        </ul>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('page_script')
+    <script>
+        $(document).ready(function() {
+            $('#kelas_siswa_table').DataTable({
+                responsive: true,
+                autoWidth: false,
+                language: {
+                    search: "Cari:",
+                    lengthMenu: "Tampilkan _MENU_ data per halaman",
+                    info: "Menampilkan _START_ hingga _END_ dari _TOTAL_ data",
+                    paginate: {
+                        first: "Pertama",
+                        last: "Terakhir",
+                        next: "Berikutnya",
+                        previous: "Sebelumnya"
+                    }
+                }
+            });
+        });
+    </script>
+@endsection
