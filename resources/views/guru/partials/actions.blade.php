41c75282af8bc75c41eee9a3bc9508a4bfebfd84
Your Name <you@example.com>
2024-11-09 00:13:23 +0700

    update actions

--- resources/views/guru/partials/actions.blade.php
+++ resources/views/guru/partials/actions.blade.php
@@ -1,13 +1,26 @@
-<button type="button" class="btn btn-info btn-sm text-white show-btn" data-id="{{ $guru->id }}"
-    data-nama="{{ $guru->nama }}" data-nip="{{ $guru->nip }}" data-kelas="{{ $guru->kelas->nama_kelas ?? '-' }}"
-    data-bs-toggle="modal" data-bs-target="#showModal">
-    <i class="fas fa-eye text-white"></i>
-</button>
-<button type="button" class="btn btn-bg-warning btn-sm edit-btn" data-id="{{ $guru->id }}"
-    data-nama="{{ $guru->nama }}" data-nip="{{ $guru->nip }}" data-kelas_id="{{ $guru->kelas_id }}"
-    data-bs-toggle="modal" data-bs-target="#editModal">
-    <i class="fas fa-edit text-white"></i>
-</button>
-<button class="btn btn-sm btn-danger delete-btn" data-id="{{ $guru->id }}">
-    <i class="fas fa-trash-alt"></i>
-</button>
+<div class="btn-group">
+    <button type="button" class="btn btn-secondary btn-sm dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        Actions
+    </button>
+    <ul class="dropdown-menu">
+        <li>
+            <button type="button" class="dropdown-item show-btn" data-id="{{ $guru->id }}"
+                data-nama="{{ $guru->nama }}" data-nip="{{ $guru->nip }}"
+                data-kelas="{{ $guru->kelas->nama_kelas ?? '-' }}" data-bs-toggle="modal" data-bs-target="#showModal">
+                <i class="fas fa-eye me-2"></i>Show
+            </button>
+        </li>
+        <li>
+            <button type="button" class="dropdown-item edit-btn" data-id="{{ $guru->id }}"
+                data-nama="{{ $guru->nama }}" data-nip="{{ $guru->nip }}" data-kelas_id="{{ $guru->kelas_id }}"
+                data-bs-toggle="modal" data-bs-target="#editModal">
+                <i class="fas fa-edit me-2"></i>Edit
+            </button>
+        </li>
+        <li>
+            <button class="dropdown-item delete-btn" data-id="{{ $guru->id }}">
+                <i class="fas fa-trash-alt me-2"></i>Delete
+            </button>
+        </li>
+    </ul>
+</div>
--- resources/views/siswa/partials/actions.blade.php
+++ resources/views/siswa/partials/actions.blade.php
@@ -1,13 +1,26 @@
-<button type="button" class="btn btn-info btn-sm text-white show-btn" data-id="{{ $siswa->id }}"
-    data-nama="{{ $siswa->nama }}" data-nis="{{ $siswa->nis }}" data-kelas="{{ $siswa->kelas->nama_kelas }}"
-    data-bs-toggle="modal" data-bs-target="#showModal">
-    <i class="fas fa-eye text-white"></i>
-</button>
-<button type="button" class="btn btn-bg-warning btn-sm edit-btn" data-id="{{ $siswa->id }}"
-    data-nama="{{ $siswa->nama }}" data-nis="{{ $siswa->nis }}" data-kelas_id="{{ $siswa->kelas_id }}"
-    data-bs-toggle="modal" data-bs-target="#editModal">
-    <i class="fas fa-edit text-white"></i>
-</button>
-<button class="btn btn-sm btn-danger delete-btn" data-id="{{ $siswa->id }}">
-    <i class="fas fa-trash-alt"></i>
-</button>
+<div class="btn-group">
+    <button type="button" class="btn btn-secondary btn-sm dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        Actions
+    </button>
+    <ul class="dropdown-menu">
+        <li>
+            <button type="button" class="dropdown-item show-btn" data-id="{{ $siswa->id }}"
+                data-nama="{{ $siswa->nama }}" data-nis="{{ $siswa->nis }}"
+                data-kelas="{{ $siswa->kelas->nama_kelas }}" data-bs-toggle="modal" data-bs-target="#showModal">
+                <i class="fas fa-eye me-2"></i>Show
+            </button>
+        </li>
+        <li>
+            <button type="button" class="dropdown-item edit-btn" data-id="{{ $siswa->id }}"
+                data-nama="{{ $siswa->nama }}" data-nis="{{ $siswa->nis }}" data-kelas_id="{{ $siswa->kelas_id }}"
+                data-bs-toggle="modal" data-bs-target="#editModal">
+                <i class="fas fa-edit me-2"></i>Edit
+            </button>
+        </li>
+        <li>
+            <button class="dropdown-item delete-btn" data-id="{{ $siswa->id }}">
+                <i class="fas fa-trash-alt me-2"></i>Delete
+            </button>
+        </li>
+    </ul>
+</div>
