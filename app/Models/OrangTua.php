808003c373978313f0d4cf90a626aec8c48830c0
Your Name <you@example.com>
2024-11-11 15:02:05 +0700

    add model n migration orang tua

---
+++ app/Models/OrangTua.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Siswa;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class OrangTua extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function siswa()
+    {
+        return $this->belongsTo(Siswa::class);
+    }
+}
